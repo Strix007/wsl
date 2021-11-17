d0ab77cf448a2518029acd1079faaffe016c8193
Strix007 <arbabashruff@gmal.com>
2021-11-18 00:10:16 +0530

    Added .config

---
+++ .config/i3/i3lock.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ICON=/home/arbab/.config/i3/lock.png
+TMPBG=/tmp/screen.png
+scrot /tmp/screen.png
+convert $TMPBG -scale 10% -scale 1000% $TMPBG
+convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
+dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause
+i3lock -u -i $TMPBG -n; dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play 
+rm /tmp/screen.png
---
+++ .config/i3/startup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Wallpaper Uitlity (Currently: Nitrogen)
+
+nitrogen --restore
+
+# Rclone
+
+rclone mount --daemon Drive_arbabashruff: ~/Mount/arbabashruff@gmail.com/
+
+# Xfce Power Manager
+
+xfce4-power-manager &
+
+# Session Utility
+
+lxsession &
+
+# Pulseaudio Volume Manager In SysTray
+
+volumeicon &
+
+# WLAN SysTray Utility Using NetworkManager (Currently: NM-Applet)
+
+nm-applet &
+
+# SysTray App For KdeConnect
+
+kdeconnect-indicator
