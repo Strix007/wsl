a7082476d8419612c0ccb42af18689fbab2e3e7c
Strix007 <arbabashruff@gmal.com>
2021-11-27 23:03:52 +0530

    Added dunst config

---
+++ .config/dunst/load.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# https://github.com/thanhvule0310/dotfiles/tree/main/.config/dunst
+pkill dunst
+dunst -config ~/.config/dunst/dunstrc &
+notify-send -a Notification -u low "Hi !" -i "/home/arbab/.config/dunst/icons/welcome.svg"
