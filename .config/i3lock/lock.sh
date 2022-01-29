20ba9ce87fe54b4264fd15881358ac36d746dfd7
Strix007 <arbabashruff@gmal.com>
2022-01-29 17:14:49 +0530

    Changed script to resume audio only if the player was active before the lock

--- .config/i3lock/lock.sh
+++ .config/i3lock/lock.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
 ICON=/home/arbab/.config/i3lock/lock.png
 TMPBG=/tmp/lock-state.png
 scrot /tmp/lock-state.png
 convert $TMPBG -scale 10% -scale 1000% $TMPBG
 convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
 playerctl -a pause
-i3lock -u -i $TMPBG -n; playerctl play
+if [ "$(playerctl status)" == "Paused" ]; then
+		i3lock -u -i $TMPBG
+    elif [ "$(playerctl status)" == "Playing" ]; then
+		i3lock -u -i $TMPBG -n; playerctl play
+
+fi
 rm /tmp/lock-state.png
