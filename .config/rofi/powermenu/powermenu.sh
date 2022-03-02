2cf73b266171c81c771f10cffe83a075ef8e8615
Strix007 <arbabashruff@gmal.com>
2022-03-02 17:03:00 +0530

    Added different exit methods for different wms

--- .config/rofi/powermenu/powermenu.sh
+++ .config/rofi/powermenu/powermenu.sh
@@ -75,7 +75,16 @@ case $chosen in
     $logout)
 		ans=$(confirm_exit &)
 		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			pkill -u $USER
+			if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
+				openbox --exit
+			elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+				bspc quit
+			elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
+				i3-msg exit
+			elif [[ "$DESKTOP_SESSION" == "xmonad" ]]; then
+				./.xmonad/xmonadctl 39
+			fi
+			
 		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
 			exit 0
         else
