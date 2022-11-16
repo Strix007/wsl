c65653957ec79febc55ac45a65ffd21afb057dc1
Strix007 <arbabashruff@gmal.com>
2022-11-16 23:52:11 +0530

    Fixed usecase where polybar wouldn't launch if  was not set, dropped unsupported charecter and moved ram module to a seperate script

--- .config/polybar/scripts/launch.sh
+++ .config/polybar/scripts/launch.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-killall -q polybar;                                  # Terminate Already Running Bar Instances
+killall -q polybar; # Terminate Already Running Bar Instances
+FALLBACKWM="xmonad" # Set fallback window manager
 
 # Launch
 
-polybar mainbar-$(echo $DESKTOP_SESSION) --config=/home/arbab/.config/polybar/config.ini &
-echo "Polybar For $(echo $DESKTOP_SESSION) Has Been Launched"
+if [ -z "$DESKTOP_SESSION" ] # Check for some reason if $DESKTOP_SESSION is empty and if so, launch polybar with a fallback window manager
+then
+    polybar mainbar-$(echo $FALLBACKWM) --config=/home/arbab/.config/polybar/config.ini &
+    echo "Polybar For $(echo $FALLBACKWM) Has Been Launched"
+else
+    polybar mainbar-$(echo $DESKTOP_SESSION) --config=/home/arbab/.config/polybar/config.ini &
+    echo "Polybar For $(echo $DESKTOP_SESSION) Has Been Launched"
+fi
+
+
