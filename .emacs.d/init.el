df8eeb6d6fb42e44e6daaa130e861ef10a82686a
Strix007 <arbabashruff@gmal.com>
2023-02-13 17:58:41 +0530

    Fix org-mode formatting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -92,16 +92,6 @@
 (set-face-attribute 'default nil        :font "JetBrains Mono" :height 125)
 (set-face-attribute 'fixed-pitch nil    :font "JetBrains Mono" :height 125)
 (set-face-attribute 'variable-pitch nil :font "Cantarell"      :height 125)
-;; Org-mode fonts
-;; Ensure that anything that should be fixed-pltch in Org files appears that way
-;; (set-face-attribute 'org-block           nil :foreground nil :inherit 'fixed-pitch)
-;; (set-face-attribute 'org-code            nil :inherit '(shadow fixed-pitch))
-;; (set-face-attribute 'org-table           nil :inherit '(shadow fixed-pitch))
-;; (set-face-attribute 'org-indent          nil :inherit '(org-hide fixed-pitch))
-;; (set-face-attribute 'org-verbatim        nil :inherit '(shadow fixed-pitch))
-;; (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-;; (set-face-attribute 'org-meta-line       nil :inherit '(font-lock-comment-face fixed-pitch))
-;; (set-face-attribute 'org-checkbox        nil :inherit 'fixed-pitch)
 
 ;; Evil
 (use-package evil
@@ -329,6 +319,30 @@
   (org-indent-mode)
   (variable-pitch-mode 1)
   (visual-line-mode 1)
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+  ;; Set faces for heading levels
+  (dolist (face '(
+		  (org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)
+		  ))
+    (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block           nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code            nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table           nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim        nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line       nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox        nil :inherit 'fixed-pitch)
   )
 
 ;; Org-mode
@@ -350,16 +364,6 @@
   (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●"))
   )
 
-;; (dolist (face '((org-level-1 . 1.2)
-;; 		(org-level-2 . 1.1)
-;; 		(org-level-3 . 1.05)
-;; 		(org-level-4 . 1.0)
-;; 		(org-level-5 . 1.1)
-;; 		(org-level-6 . 1.1)
-;; 		(org-level-7 . 1.1)
-;; 		(org-level-8 . 1.1)
-;; 		))
-;;   (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
 
 ;; Visual-fill-column
 (use-package visual-fill-column
