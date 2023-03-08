c6829bcd841b55f97a3fe9f268547cc1c4e0fc70
Arbab Khan <arbabashruff@gmail.com>
2023-03-08 22:38:30 +0530

    Removed profile from chemacs and added loop for folders in mpv

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -3,6 +3,5 @@
  ("centaur"   . ((user-emacs-directory . "~/centaur-emacs")))
  ("doom"      . ((user-emacs-directory . "~/doom-emacs")))
  ("spacemacs" . ((user-emacs-directory . "~/spacemacs")))
- ("yay-evil"  . ((user-emacs-directory . "~/yay-evil-emacs")))
  ("prelude"   . ((user-emacs-directory . "~/prelude-emacs")))
   )
