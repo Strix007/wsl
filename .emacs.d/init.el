ea9d5759126c6be5418bae58688d9e64c80f55a6
Arbab Khan <arbabashruff@gmail.com>
2023-05-20 23:51:04 +0530

    Add comment for use-package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,12 +18,13 @@
   (package-refresh-contents)
   )
 
+;; Use-package
 ;; Initialize use-package on non-Linux platforms
 (unless (package-installed-p 'use-package)
   (package-install 'use-package)
   )
-
 (require 'use-package)
+
 (setq use-package-always-ensure t)
 (setq use-package-verbose nil)
 
