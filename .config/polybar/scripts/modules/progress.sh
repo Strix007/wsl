46097d9d047ced00f16f2774b0882fe6a682b0df
Arbab Khan <arbabashruff@gmail.com>
2023-06-21 01:08:06 +0530

    Add new progress module

---
+++ .config/polybar/scripts/modules/progress.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while true
+do
+    PROGRESS="$(progress 2>/dev/null | sed -n "2 p" | awk '{print $1}')"
+    if [[ -z $PROGRESS ]]; then
+        echo ""
+    else
+        echo "|  $PROGRESS"
+    fi
+done
