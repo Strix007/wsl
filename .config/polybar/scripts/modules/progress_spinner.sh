ec14f7255aeeeee3864699c52d2cd223ff302154
Arbab Khan <arbabashruff@gmail.com>
2023-06-25 23:24:14 +0530

    Forgot arrays existed

--- .config/polybar/scripts/modules/progress_spinner.sh
+++ .config/polybar/scripts/modules/progress_spinner.sh
@@ -1,49 +1,17 @@
 #!/bin/bash
 
+mySpinners=("⠋" "⠙" "⠹" "⠸" "⠼" "⠴" "⠦" "⠧" "⠇" "⠏")
+
 while true
 do
     PROGRESS="$(progress 2>/dev/null | sed -n "2 p" | awk '{print $1}')"
-    if [[ -z $PROGRESS ]]; then
+    if [[ -z "$PROGRESS" ]]; then
         echo ""
     else
-        for i in {1..10}
+        for i in "${mySpinners[@]}"
         do
             sleep 0.1
-            case $i in
-                1)
-                    echo "| ⠋"
-                    ;;
-                2)
-                    echo "| ⠙"
-                    ;;
-                3)
-                    echo "| ⠹"
-                    ;;
-                4)
-                    echo "| ⠸"
-                    ;;
-                5)
-                    echo "| ⠼"
-                    ;;
-                6)
-                    echo "| ⠴"
-                    ;;
-                7)
-                    echo "| ⠦"
-                    ;;
-                8)
-                    echo "| ⠧"
-                    ;;
-                9)
-                    echo "| ⠇"
-                    ;;
-                10)
-                    echo "| ⠏"
-                    ;;
-                *)
-                    echo "Unexpected case!"
-                    ;;
-            esac
+            echo "$i"
         done
     fi
 done
